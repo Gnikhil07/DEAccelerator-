1fef05edad453ed5f6636b8bead9d8199ef089b8
Gnikhil07 <65018585+Gnikhil07@users.noreply.github.com>
2020-06-26 09:49:45 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -99,10 +99,10 @@ def home():
         #past runs
         cursor.execute(" select *  from audittable   WHERE  UserName=%s AND Status!=%s order by EntryID desc limit 5; ",(account,'Running'))
         data1 = cursor.fetchall() 
-        df2 = pd.DataFrame(data1, columns=[ 'EntryID','Job Name', 'Project Name', 'Start Time', 'End Time', 'UserName', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Rejected Rows', 'Status', 'Relative File Path'])
+        df2 = pd.DataFrame(data1, columns=[ 'EntryID','Job Name', 'Project Name', 'Start Time', 'End Time', 'UserName', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Business Rule Check Failed','Rejected Rows', 'Status', 'Relative File Path'])
         #df2 = pd.DataFrame(data1, columns=['JobName','RunID' ,'StartTime' ,'EndTime','UserName' ,'TotalRows' ,'DuplicateRows','DuplicatePrimaryKey','DQCheckFailed','Status','RelativeFilePath','ProjectName'])
         df3=  df2.drop(['UserName','EntryID'], axis = 1)
-        columns_order=[ 'Project Name','Job Name', 'Start Time', 'End Time', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Rejected Rows', 'Status', 'Relative File Path']
+        columns_order=[ 'Project Name','Job Name', 'Start Time', 'End Time', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Business Rule Check Failed','Rejected Rows', 'Status', 'Relative File Path']
         df4 = df3.reindex(columns=columns_order)
         # Show the profile page with account info
         return render_template('home.html',column_names=df.columns.values, row_data=list(df.values.tolist()), zip=zip,column_names1=df4.columns.values, row_data1=list(df4.values.tolist()), zip1=zip, account=account, clients = clients, Project = Project,Owner = Owner)
@@ -139,11 +139,12 @@ def homeSearch():
 
         cursor.execute(" select *  from audittable   WHERE  ProjectName LIKE %s AND UserName=%s AND Status!='Running' order by EntryID desc limit 5;",(search,account))
         data1 = cursor.fetchall() 
-        df2 = pd.DataFrame(data1, columns=[ 'EntryID','Job Name', 'Project Name', 'Start Time', 'End Time', 'UserName', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Rejected Rows', 'Status', 'Relative File Path'])
+        df2 = pd.DataFrame(data1, columns=[ 'EntryID','Job Name', 'Project Name', 'Start Time', 'End Time', 'UserName', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Business Rule Check Failed','Rejected Rows', 'Status', 'Relative File Path'])
         #df2 = pd.DataFrame(data1, columns=['JobName','RunID' ,'StartTime' ,'EndTime','UserName' ,'TotalRows' ,'DuplicateRows','DuplicatePrimaryKey','DQCheckFailed','Status','RelativeFilePath','ProjectName'])
         df3=  df2.drop(['UserName','EntryID'], axis = 1)
-        columns_order=[ 'Project Name','Job Name', 'Start Time', 'End Time', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Rejected Rows', 'Status', 'Relative File Path']
+        columns_order=[ 'Project Name','Job Name', 'Start Time', 'End Time', 'Total Rows', 'Ingested Rows', 'Duplicate Records', 'DQ Check Failed','Business Rule Check Failed','Rejected Rows', 'Status', 'Relative File Path']
         df4 = df3.reindex(columns=columns_order)
+       
         mydb.commit()
         cursor.close()
         return render_template("home.html", column_names=df.columns.values, row_data=list(df.values.tolist()), zip=zip,column_names1=df4.columns.values, row_data1=list(df4.values.tolist()), zip1=zip,account=account, clients = clients, Project = Project,Owner = Owner)
@@ -357,6 +358,38 @@ def get_confirm_token(response):
             return None
 
 
+def Business_Description(instr):
+    shdict = {'%': 'Percentage','rev':'Revenue','cat':'Category','num': 'Number','#': 'Number','no': 'Number','Sl': 'Serial','ph': 'Phone', 'idx': 'Index','dob': 'Date of Birth','perc':'Percentage','cust':'Customer'}
+    instr = str(instr)
+    import re
+    tempstr = instr.replace("_"," ")
+    tempstr = tempstr.replace("."," ")
+    tempstr = re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ',tempstr)
+    tempstr = tempstr.lower()
+    for word in tempstr.split():
+        try:
+            tempstr = tempstr.replace(word,shdict[word])
+        except:
+            tempstr
+    if(instr.isupper()):
+        tempstr = tempstr.upper() 
+    else:
+        tempstr = tempstr.title()
+    return tempstr
+
+def pii(instr):
+    import re
+    if (bool(re.match(r'^(?!000|.+0{4})(?:\d{9}|\d{3}-\d{2}-\d{4})$',str(instr)))):
+        return "Social Security Number"
+    elif(bool(re.match(r'^(?:\d{10}|\d{3}-\d{3}-\d{4})$',str(instr)))):
+        return "Phone Number"
+    elif(bool(re.match(r'\b[A-Z0-9.@_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b',str(instr),re.I))):
+        return "Email ID"
+    elif(bool(re.match(r'^([456][0-9]{3})-?([0-9]{4})-?([0-9]{4})-?([0-9]{4})$',str(instr)))):
+        return "Credit Card Number"
+    else:
+        return "None"
+
 @app.route('/metadata', methods=['GET', 'POST'])
 def index():                                     # for flat file and mysql metadata
     SourceType = session['source location type']
@@ -369,13 +402,21 @@ def index():                                     # for flat file and mysql metad
         data = cursor.fetchall() 
         df = pd.DataFrame(data, columns=['Column Name' ,'Data Type' ,'Nullable' ,'Primary Key' ,'Default' ,'Description'])
         df = df.assign(ColumnNumber=[i+1 for i in range(len(df))])[['ColumnNumber'] + df.columns.tolist()]
-        df1=  df.drop(['Primary Key'], axis = 1)
-        df2= df1.drop(['Nullable'], axis = 1)
-        df3 = df2.drop(['Description'], axis = 1)
-        df4 = df3.drop(['Default'], axis = 1)
+        df4=  df.drop(['Primary Key','Nullable','Description','Default'], axis = 1)
         df4['Column Number']= df4['ColumnNumber']
         df5 = df4.drop(['ColumnNumber'], axis = 1)
-        columns_order=['Column Number','Column Name','Data Type']
+        df5['Description'] = df5["Column Name"].map(lambda row: Business_Description(row)) 
+        cursor.execute(session['source query']+" LIMIT 100 ")
+        data2= cursor.fetchall()
+        df_temp = pd.DataFrame(data2,columns=df5["Column Name"].tolist())
+        adict = {}
+        for col in df_temp.columns.tolist():
+            df_temp[col] = df_temp[col].map(lambda ele:pii(ele))
+            x = df_temp[col].value_counts().idxmax()
+            adict[col]= x
+        df5["PII Type"] = df5["Column Name"].map(lambda ele :adict[ele])
+        df5["PII"] = df5["PII Type"].map(lambda ele:"No" if ele  =="None"  else "Yes" )
+        columns_order=['Column Number','Column Name','Data Type','Description',"PII","PII Type"]
         df6 = df5.reindex(columns=columns_order)
         cursor.execute(" DROP VIEW temp ")
         mydb.commit()
@@ -412,7 +453,15 @@ def index():                                     # for flat file and mysql metad
         df3 = df3.assign(ColumnNumber=[i+1 for i in range(len(df3))])[['ColumnNumber'] + df3.columns.tolist()]
         df3['Column Number']= df3['ColumnNumber']
         df4 = df3.drop(['ColumnNumber'], axis = 1)
-        columns_order=['Column Number','Column Name','Data Type']
+        df4['Description'] = df4["Column Name"].map(lambda row: Business_Description(row))  
+        adict = {}
+        for col in df.columns.tolist():
+            df[col] = df[col].map(lambda ele:pii(ele))
+            x = df[col].value_counts().idxmax()
+            adict[col]= x
+        df4["PII Type"] = df4["Column Name"].map(lambda ele :adict[ele])
+        df4["PII"] = df4["PII Type"].map(lambda ele:"No" if ele  =="None"  else "Yes" )
+        columns_order=['Column Number','Column Name','Data Type','Description',"PII","PII Type"]
         df5 = df4.reindex(columns=columns_order)
         value = session['file exists']
         account = session['username']
@@ -436,6 +485,7 @@ def index():                                     # for flat file and mysql metad
             a=[]
             for row in islice(reader,0,5):
                 #print(row)
+                
                 for cell in row:
                     y=conv2(cell)
                     a.append(y)
@@ -450,7 +500,15 @@ def index():                                     # for flat file and mysql metad
         df3 = df3.assign(ColumnNumber=[i+1 for i in range(len(df3))])[['ColumnNumber'] + df3.columns.tolist()]
         df3['Column Number']= df3['ColumnNumber']
         df4 = df3.drop(['ColumnNumber'], axis = 1)
-        columns_order=['Column Number','Column Name','Data Type']
+        df4['Description'] = df4["Column Name"].map(lambda row: Business_Description(row))  
+        adict = {}
+        for col in df.columns.tolist():
+            df[col] = df[col].map(lambda ele:pii(ele))
+            x = df[col].value_counts().idxmax()
+            adict[col]= x
+        df4["PII Type"] = df4["Column Name"].map(lambda ele :adict[ele])
+        df4["PII"] = df4["PII Type"].map(lambda ele:"No" if ele  =="None"  else "Yes" )
+        columns_order=['Column Number','Column Name','Data Type','Description',"PII","PII Type"]
         df5 = df4.reindex(columns=columns_order)
         value = session['file exists']
         account = session['username']
@@ -483,7 +541,15 @@ def index():                                     # for flat file and mysql metad
         df3 = df3.assign(ColumnNumber=[i+1 for i in range(len(df3))])[['ColumnNumber'] + df3.columns.tolist()]
         df3['Column Number']= df3['ColumnNumber']
         df4 = df3.drop(['ColumnNumber'], axis = 1)
-        columns_order=['Column Number','Column Name','Data Type']
+        df4['Description'] = df4["Column Name"].map(lambda row: Business_Description(row))  
+        adict = {}
+        for col in df.columns.tolist():
+            df[col] = df[col].map(lambda ele:pii(ele))
+            x = df[col].value_counts().idxmax()
+            adict[col]= x
+        df4["PII Type"] = df4["Column Name"].map(lambda ele :adict[ele])
+        df4["PII"] = df4["PII Type"].map(lambda ele:"No" if ele  =="None"  else "Yes" )
+        columns_order=['Column Number','Column Name','Data Type','Description',"PII","PII Type"]
         df5 = df4.reindex(columns=columns_order)
         value = session['file exists']
         account = session['username']
@@ -502,10 +568,22 @@ def Rollbackmetadata():
     else:
         return redirect(url_for('index'))    
 
+@app.route('/Rollbackbusinessmetadata', methods=['GET', 'POST'])
+def Rollbackbusinessmetadata():
+    mydb = mysql.connector.connect(host="demetadata.mysql.database.azure.com",user="DEadmin@demetadata",passwd="Tredence@123",database = "deaccelator")
+    cursor = mydb.cursor()
+    cursor.execute("DELETE FROM business_rule_metadata WHERE EntryID = %s ;"%(session['EntryID']))
+    mydb.commit()
+    cursor.close()
+    return redirect(url_for('rules'))
+
+
+
 
 @app.route('/ingest', methods=['GET', 'POST'])
 def index1():
     if request.method == "POST":
+        session['rule exists']="No"
         newform = request.form.getlist
         # EntryID = newform('EntryID')
         ColumnNumber = newform('Column Number')
@@ -517,10 +595,12 @@ def index1():
         Column_description = newform('Description')
         DQ_Check = newform('DQ Check')
         Date_Format = newform('Date Format')
+        PII = newform("PII")
+        PIIType = newform("PII Type")
         mydb = mysql.connector.connect(host="demetadata.mysql.database.azure.com",user="DEadmin@demetadata",passwd="Tredence@123",database = "deaccelator")
         cursor = mydb.cursor()
-        df4 = pd.DataFrame(list(zip(ColumnNumber,ColumnName,DataType,PrimaryKey,Nullable,DQ_Check,Default,Date_Format,Column_description)), columns =['ColumnNumber','ColumnName','DataType','PrimaryKey','Nullable','DQCheck','Default','Format','Description'])
-        df8 = pd.DataFrame(list(zip(ColumnNumber,ColumnName,DataType,PrimaryKey,Nullable,DQ_Check,Default,Date_Format,Column_description)), columns =['Column Number','Column Name','Data Type','Primary Key','Nullable','DQ Check','Default','Date Format','Description'])
+        df4 = pd.DataFrame(list(zip(ColumnNumber,ColumnName,DataType,PrimaryKey,Nullable,DQ_Check,Default,Date_Format,Column_description,PII,PIIType)), columns =['ColumnNumber','ColumnName','DataType','PrimaryKey','Nullable','DQCheck','Default','Format','Description','PII','PIIType'])
+        df8 = pd.DataFrame(list(zip(ColumnNumber,ColumnName,DataType,PrimaryKey,Nullable,DQ_Check,Default,Date_Format,Column_description,PII,PIIType)), columns =['Column Number','Column Name','Data Type','Primary Key','Nullable','DQ Check','Default','Date Format','Description','PII','PII Type'])
         df1 = df4.assign(EntryID=session['EntryID'])[['EntryID'] + df4.columns.tolist()]
         # session['json_metadata']=str(df1.to_json)
         # print(session['json_metadata'])
@@ -546,7 +626,8 @@ def index1():
                 account = session['username']
                 return render_template("metadata2.html", column_names=df8.columns.values, row_data=list(df8.values.tolist()), zip=zip, value=value,account = account )
             else :
-                value = 'YES, But Metadata is not matching'
+                session['file exists'] = 'YES, But Metadata is not matching'
+                value = session['file exists']
                 account = session['username']
                 return render_template("metadata2.html", column_names=df8.columns.values, row_data=list(df8.values.tolist()), zip=zip, value=value,account=account)
             
@@ -556,7 +637,102 @@ def index1():
             return render_template("metadata2.html", column_names=df8.columns.values, row_data=list(df8.values.tolist()), zip=zip, value=value,account = account)
     return render_template("metadata.html",account = account)
 
+@app.route('/rules', methods=['GET', 'POST'])
+def rules():
+    mydb = mysql.connector.connect(host="demetadata.mysql.database.azure.com",user="DEadmin@demetadata",passwd="Tredence@123",database = "deaccelator")
+    cursor = mydb.cursor()
+    cursor.execute("SELECT concat(ColumnName,'`~`',Description) as x FROM metadata WHERE EntryID = %s ;"%(session['EntryID']))
+    data = cursor.fetchall()
+    data1= list(data)
+    cursor.execute("SELECT Name as x FROM centralrulerepo")
+    data2 = cursor.fetchall()
+    data3= list(data2)
+    
+    #print(data1)
+    out = [item for t in data1 for item in t] 
+    out1 = [item for t in data3 for item in t]
+    # printing output 
+    #print(out) 
+    mydb.commit()
+    cursor.close()
+    value = session['file exists']
+    account = session['username']
+    if session['rule exists']=="Yes":
+        msg="Rule Name already Taken!"
+    elif session['rule exists']=="Inserted":
+        msg="New Rule "+session['RuleName']+" Added"
+    elif session['rule exists']=="No":
+        msg=""
+    return render_template("metadata3.html", data = out,data1=out1,account = account,value=value,msg=msg)
 
+@app.route('/rules01', methods=['GET', 'POST'])
+def rules01():
+    if request.method == "POST":
+        details = request.form
+        RuleName=details['Rule Name']
+        session['RuleName']=RuleName
+        RuleDefinition=details['Rule Definition']
+        RuleCreator=details['Rule Creator']
+        RuleApprover=details['Rule Approver']
+        mydb = mysql.connector.connect(host="demetadata.mysql.database.azure.com",user="DEadmin@demetadata",passwd="Tredence@123",database = "deaccelator")
+        cursor = mydb.cursor()
+        cursor.execute('SELECT * FROM CentralRuleRepo WHERE `Name` = %s And UserName=%s ',(RuleName,session['username']))
+        account = cursor.fetchone()
+        if account:
+            session['rule exists']="Yes"
+            return redirect(url_for('rules'))
+        else:
+            cursor.execute('INSERT INTO CentralRuleRepo (`Name`,UserName,`Definition`,Creator,Approver) VALUES(%s,%s,%s,%s,%s) ',(RuleName,session['username'],RuleDefinition,RuleCreator,RuleApprover))
+            mydb.commit()
+            cursor.close()
+            session['rule exists']="Inserted"
+            return redirect(url_for('rules'))
+
+
+
+
+
+@app.route('/rules1', methods=['GET', 'POST'])
+def rules1():
+    if request.method == "POST":
+        newform = request.form.getlist
+        ColumnName = newform('Cname')
+        RuleName = newform('Rule')
+        Parameters = newform('Para')
+        print(ColumnName)
+        df4 = pd.DataFrame(list(zip(ColumnName,RuleName,Parameters)), columns =['Column Name','RuleName','RuleParameters'])
+        print(df4)
+        df4['ColumnName']=df4['Column Name'].map(lambda x: x.split("`~`",1)[0])
+        df4['Description']=df4['Column Name'].map(lambda x: x.split("`~`",1)[1])
+        df5=df4.drop(['Column Name'], axis = 1)
+        print(df5)
+        columns_order=['ColumnName','Description','RuleName','RuleParameters']
+        df6 = df5.reindex(columns=columns_order)
+        df1 = df6.assign(EntryID=session['EntryID'])[['EntryID'] + df6.columns.tolist()]
+        df1['RuleName']=df1['RuleName'].map(lambda x: x.replace(" ",""))
+
+        mydb = mysql.connector.connect(host="demetadata.mysql.database.azure.com",user="DEadmin@demetadata",passwd="Tredence@123",database = "deaccelator")
+        cursor = mydb.cursor()
+        cursor.execute(" SELECT  `Key` FROM userdetails WHERE `UserName`=%s AND `Status`=%s ; ",(session['username'],1))
+        data = cursor.fetchone()
+        a=data[0]
+        print(data[0])
+        df1.loc[df1.RuleName == 'Encrypt', 'RuleParameters'] = a
+        print(df1)
+        cols = "`,`".join([str(i) for i in df1.columns.tolist()])
+        for i,row in df1.iterrows():
+            sql = "INSERT INTO `business_rule_metadata` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+            cursor.execute(sql, tuple(row))
+        mydb.commit()
+        cursor.close()
+        cursor = mydb.cursor()
+        statement = 'select a.`ColumnName`,a.`DataType`,a.`PrimaryKey`,a.`Nullable`,a.`DQCheck`,a.`Default`,a.`Format`,a.`Description`,a.`PII`,a.`PIIType`,b.`Rules` from (select * from `deaccelator`.`metadata` where `EntryID` = %s) as a left join (select `ColumnName`, group_concat(`RuleName`," - " ,`RuleParameters`) as `Rules` from `deaccelator`.`business_rule_metadata` where `EntryID` =%s group by `ColumnName`) as b on a.`ColumnName` = b.`ColumnName`;'%(session['EntryID'],session['EntryID'])
+        cursor.execute(statement)
+        data1 = cursor.fetchall()
+        columnlist = ['Column Name','Data Type','Primary Key','Nullable','DQ Check','Default','Format','Description','PII','PII Type','Rules']
+        df8 = pd.DataFrame(data1,columns = columnlist)
+        cursor.close()
+        return render_template("metadata4.html", column_names=df8.columns.values, row_data=list(df8.values.tolist()), zip=zip,value=session['file exists'])
 
 import requests,json
 
